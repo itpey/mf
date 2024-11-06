84edff1af983950d3ffd7de128b225272ebf5221
itpey <52120725+itpey@users.noreply.github.com>
2024-11-06 18:48:45 +0330

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"runtime"
+	"strings"
 )
 
 func main() {
@@ -12,6 +15,11 @@ func main() {
 	}
 
 	fileName := os.Args[1]
+
+	if err := validateFilename(fileName); err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_EXCL, 0666)
 	if err != nil {
@@ -25,4 +33,40 @@ func main() {
 	defer file.Close()
 
 	fmt.Printf("File '%s' created successfully.\n", fileName)
+}
+
+func validateFilename(fileName string) error {
+
+	if fileName == "" {
+		return fmt.Errorf("filename cannot be empty")
+	}
+
+	if runtime.GOOS == "windows" {
+
+		invalidChars := `[<>:"/\\|?*]`
+		re := regexp.MustCompile(invalidChars)
+		if re.MatchString(fileName) {
+			return fmt.Errorf("filename contains invalid characters for Windows")
+		}
+
+		if strings.HasSuffix(fileName, " ") || strings.HasSuffix(fileName, ".") {
+			return fmt.Errorf("filename cannot end with a space or period on Windows")
+		}
+	} else {
+		invalidChars := `[\x00/]`
+		re := regexp.MustCompile(invalidChars)
+		if re.MatchString(fileName) {
+			return fmt.Errorf("filename contains invalid characters (null byte or /) for Unix-based systems")
+		}
+	}
+
+	if len(fileName) > 255 {
+		return fmt.Errorf("filename is too long (max 255 characters)")
+	}
+
+	if strings.TrimSpace(fileName) == "" {
+		return fmt.Errorf("filename cannot be just spaces")
+	}
+
+	return nil
 }
